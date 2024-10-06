dbcc29214d5ef6afc8f9e9ca84673254a59d7711
Moeen Mahmud <write2fahim007@gmail.com>
2024-10-06 18:51:36 +0600

    testing husky

--- src/components/SlideBarTab/SlideBarTab.tsx
+++ src/components/SlideBarTab/SlideBarTab.tsx
@@ -13,7 +13,6 @@ import {
 } from "@react-native-floating-tab";
 import { useEffect, useMemo, useState } from "react";
 import { Platform, StyleSheet, View } from "react-native";
-
 import Animated, { useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated";
 
 import { SlideBarTabButton } from "./SlideBarTabButton";
